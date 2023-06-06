71bffe89d66178f3d329880f44b3f3b99f470ede
EdPaul1 <saidarajab101@gmail.com>
2023-06-06 19:30:26 +0300

    Application controller

--- Bookstore-Backend/app/controllers/application_controller.rb
+++ Bookstore-Backend/app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < Sinatra::Base
-    set :default_content_type, 'application/json'
-    
-    
-    
-  end
+  set :default_content_type, 'application/json'
+  
+  get '/books'do 
+  book = Book.all
+  book.to_json
+ end
+  
+  
+end
