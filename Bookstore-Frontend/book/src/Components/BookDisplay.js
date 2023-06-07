5fcd46afb4b6eeeb89a487043d033f8296592c7f
Komenpurity <jepkorirpurity8@gmail.com>
2023-06-07 10:19:49 +0300

    finised the header

--- Bookstore-Frontend/book/src/Components/BookDisplay.js
+++ Bookstore-Frontend/book/src/Components/BookDisplay.js
@@ -6,7 +6,7 @@ export default function BookDisplay({image,author,description,id,handleCart}) {
                 <img className="card-img-top img-thumbnail " src={image} alt="Card image cap"/>
                 <div className="card-body">
                 <h5 className="card-title text-muted">{author}</h5>
-                <p className="card-text">{description}</p>
+                {/* <p className="card-text">{description}</p> */} 
                 <button type="button" onClick={() => handleCart(id)}  className="btn btn-outline-success m-2">Add to Cart</button> 
                 <button type="button" className="btn btn-outline-success">Purchase</button>
                 </div>
