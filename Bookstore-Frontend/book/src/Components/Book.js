425dd1f8025e247b8bf5fec79fae29344f6373f9
Komenpurity <jepkorirpurity8@gmail.com>
2023-06-07 14:06:41 +0300

    added server link endpoint

--- Bookstore-Frontend/book/src/Components/Book.js
+++ Bookstore-Frontend/book/src/Components/Book.js
@@ -7,11 +7,11 @@ export default function Book() {
     const [book,setBook] = useState([])  
 
     useEffect(() => {
-        fetch("https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=USLX52voDPN51lzOHNAI3HC5jGGKxcSG")
+        fetch("http://localhost:9292/")
         .then(response => response.json())
         .then(data => {
-            setBook(data.results.books) 
-            console.log(data.results.books) 
+            setBook(data) 
+            console.log(data) 
         })  
     },[] )
     //console.log(book.results) 
@@ -25,7 +25,7 @@ export default function Book() {
             <Header/>  
         <div className='row'> 
             {book.map((b) => { 
-                return <BookDisplay handleCart={handleCart} id={b.rank} amazon={b.amazon_product_url} image={b.book_image} author={b.author} description={b.description}/>  
+                return <BookDisplay handleCart={handleCart} id={b.id} image={b.book_image} author={b.author} title={b.title} />  
             })}
          </div>
 
--- Bookstore-Frontend/book/src/Components/BookDisplay.js
+++ Bookstore-Frontend/book/src/Components/BookDisplay.js
@@ -1,14 +1,13 @@
 import React from 'react'
 
-export default function BookDisplay({image,author,description,id,handleCart}) {
+export default function BookDisplay({image,author,title,id,handleCart}) {
   return (
             <div className="col-2 card m-3" key={id}>  
-                <img className="card-img-top img-thumbnail " src={image} alt="Card image cap"/>
+                <img className="card-img-top img-thumbnail " src={image} alt="cover"/>
                 <div className="card-body">
-                <h5 className="card-title text-muted">{author}</h5>
-                {/* <p className="card-text">{description}</p> */} 
+                <h5 className="card-title">{author}</h5>
+                <p className="card-text text-muted">{title}</p> 
                 <button type="button" onClick={() => handleCart(id)}  className="btn btn-outline-success m-2">Add to Cart</button> 
-                <button type="button" className="btn btn-outline-success">Purchase</button>
                 </div>
             </div>
   )
