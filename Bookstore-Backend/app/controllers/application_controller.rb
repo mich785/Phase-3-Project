a4458551c94cc53c3384dee431f3c19ca7de37b3
EdPaul1 <126860107+EdPaul1@users.noreply.github.com>
2023-06-07 14:24:23 +0300

    Update application_controller.rb

--- Bookstore-Backend/app/controllers/application_controller.rb
+++ Bookstore-Backend/app/controllers/application_controller.rb
@@ -1,70 +1,78 @@
 class ApplicationController < Sinatra::Base
-    set :default_content_type, 'application/json'
-    
-    get '/'do 
-    book = Book.all
-    book.to_json
-   end
+  set :default_content_type, 'application/json'
+  
+  get '/' do
+    books = Book.all
+    books.to_json
+  end
 
-
-   get'/reviews'do
-   review = Review.all
-   review.to_json
+  get '/reviews' do
+    reviews = Review.all
+    reviews.to_json
   end
   
-  post'/reviews'do
-   review = Review.create(comment: params[:comment],user_id: params[:user_id],book_id:params[:book_id])
+  post '/reviews' do
+    review = Review.create(comment: params[:comment], user_id: params[:user_id], book_id: params[:book_id])
+    review.to_json
   end
 
   get '/reviews/:id' do
     review = Review.find(params[:id])
- review.to_json
-end
-
-patch '/reviews/:id' do
-  review = Review.find(params[:id])
-  review.update(comment: params[:comment])
-  
-  
-
-delete '/reviews/:id' do
- review.to_json
-end
-
-get '/books/:id' do
-  book = Book.find(params[:id])
-  book.to_json
-end
-
-patch '/books/:id' do
-    book = Book.find(params[:id])
- book.update(title: params[:title],
-    author: params[:author],
-     description: params[:description],
-    genre: params[:genre],
-     publisher: params[:publisher])
-end
-
-delete '/books/:id' do
-  book = Book.find(params[:id])
-  book.to_json
-end
-
-get '/users/:id' do
-    user = User.find(params[:id])
- user.to_json
- end
-
-end
-
-patch '/users/:id' do
-    user = User.find(params[:id])
-    user.update(username: params[:username],
-    password: params[:password])
-    end
-
-delete '/users/:id' do
-end
-    
+    review.to_json
   end
 
+  patch '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.update(comment: params[:comment])
+    review.to_json
+  end
+  
+  delete '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.destroy
+    review.to_json
+  end
+
+  get '/books/:id' do
+    book = Book.find(params[:id])
+    book.to_json
+  end
+
+  patch '/books/:id' do
+    book = Book.find(params[:id])
+    book.update(
+      title: params[:title],
+      author: params[:author],
+      description: params[:description],
+      genre: params[:genre],
+      publisher: params[:publisher]
+    )
+    book.to_json
+  end
+
+  delete '/books/:id' do
+    book = Book.find(params[:id])
+    book.destroy
+    book.to_json
+  end
+
+  get '/users/:id' do
+    user = User.find(params[:id])
+    user.to_json
+  end
+
+  patch '/users/:id' do
+    user = User.find(params[:id])
+    user.update(
+      username: params[:username],
+      password: params[:password]
+    )
+    user.to_json
+  end
+
+  delete '/users/:id' do
+    user = User.find(params[:id])
+    user.destroy
+    user.to_json
+  end
+end
